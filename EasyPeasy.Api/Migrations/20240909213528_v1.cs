6c86b958bfec88597b47ea895d2732df329b6266
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-09 18:53:00 -0300

    feat: database migration

---
+++ EasyPeasy.Api/Migrations/20240909213528_v1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EasyPeasy.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    UserId = table.Column<string>(type: "VARCHAR", maxLength: 160, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FinancialMovements",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric(12,6)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP", nullable: false),
+                    Type = table.Column<short>(type: "SMALLINT", nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false),
+                    CategotyId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<string>(type: "VARCHAR", maxLength: 160, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FinancialMovements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FinancialMovements_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FinancialMovements_CategoryId",
+                table: "FinancialMovements",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FinancialMovements");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
