670bdc8f85e80032c633d16840af50173ad3c3f5
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-09 18:48:54 -0300

    feat: create financial movement model

---
+++ EasyPeasy.Api/Domain/FinancialMovement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EasyPeasy.Api.Domain.Enums;
+
+namespace EasyPeasy.Api.Domain;
+
+public class FinancialMovement : EntityBase
+{
+    public string Title { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public EMovementType Type { get; set; } = EMovementType.EXPENSE;
+    public Category Category { get; set; } = null!;
+    public long CategotyId { get; set; }
+    public string UserId { get; set; }
+}
+
