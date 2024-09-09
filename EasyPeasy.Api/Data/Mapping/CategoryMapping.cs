45edf5a8f697a4671e044fa7448ff03f0ccb1076
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-09 18:41:03 -0300

    feat: create category model

---
+++ EasyPeasy.Api/Data/Mapping/CategoryMapping.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EasyPeasy.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyPeasy.Api.Data.Mapping;
+
+public class CategoryMapping : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasColumnType("TEXT")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Description)
+            .IsRequired()
+            .HasColumnType("TEXT")
+            .HasMaxLength(255);
+
+        builder.Property(x => x.UserId)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+    }
+}
+
---
+++ EasyPeasy.Api/Domain/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EasyPeasy.Api.Domain;
+
+public class Category : EntityBase
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
+
