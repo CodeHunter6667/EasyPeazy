69d78c0aa57f2507e21b478462b882090adac9f0
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-10 17:45:58 -0300

    feat: create mapping of financial movement

---
+++ EasyPeasy.Api/Data/Mapping/FinancialMovementMapping.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using EasyPeasy.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyPeasy.Api.Data.Mapping;
+
+public class FinancialMovementMapping : IEntityTypeConfiguration<FinancialMovement>
+{
+    public void Configure(EntityTypeBuilder<FinancialMovement> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Title)
+            .IsRequired()
+            .HasColumnType("TEXT")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Amount)
+            .IsRequired()
+            .HasColumnType("DECIMAL(12,6)");
+
+        builder.Property(x => x.CreatedAt)
+            .IsRequired()
+            .HasColumnType("TIMESTAMP");
+
+        builder.Property(x => x.Type)
+            .IsRequired()
+            .HasColumnType("SMALLINT");
+
+        builder.Property(x => x.UserId)
+            .IsRequired()
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(160);
+    }
+}
+
