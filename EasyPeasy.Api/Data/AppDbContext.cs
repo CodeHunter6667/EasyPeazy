97a5a1828bff0f93101ee227faa26312261990fd
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-10 17:46:43 -0300

    feat: create app db context

---
+++ EasyPeasy.Api/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using EasyPeasy.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyPeasy.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<FinancialMovement> FinancialMovements { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
