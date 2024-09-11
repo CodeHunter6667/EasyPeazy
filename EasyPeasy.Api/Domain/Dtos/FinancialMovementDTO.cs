ce5324b03a1c2e18ba4dca9b4b726e86bea5a160
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-11 18:39:09 -0300

    feat: create FinancialMovementDTO

---
+++ EasyPeasy.Api/Domain/Dtos/FinancialMovementDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EasyPeasy.Api.Domain.Enums;
+
+namespace EasyPeasy.Api.Domain.Dtos;
+
+public class FinancialMovementDTO
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public EMovementType Type { get; set; }
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
