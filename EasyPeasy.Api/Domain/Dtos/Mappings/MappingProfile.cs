d62d0f05b6fd806f33c5e000fdc8cdd9257a9ca4
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-11 18:40:32 -0300

    feat: create MappingProfile

---
+++ EasyPeasy.Api/Domain/Dtos/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AutoMapper;
+
+namespace EasyPeasy.Api.Domain.Dtos.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CategoryDTO, Category>().ReverseMap();
+            CreateMap<FinancialMovementDTO, FinancialMovement>().ReverseMap();
+        }
+    }
+}
+
