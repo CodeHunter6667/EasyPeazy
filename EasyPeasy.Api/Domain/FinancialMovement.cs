0c626ae2eaffef455b69a195b4546d4fc3a7ef54
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-11 18:38:52 -0300

    feat: CreatedAt as datetime.now

--- EasyPeasy.Api/Domain/FinancialMovement.cs
+++ EasyPeasy.Api/Domain/FinancialMovement.cs
@@ -7,10 +7,9 @@ public class FinancialMovement : EntityBase
 {
     public string Title { get; set; } = string.Empty;
     public decimal Amount { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
     public EMovementType Type { get; set; } = EMovementType.EXPENSE;
     public Category Category { get; set; } = null!;
     public long CategotyId { get; set; }
     public string UserId { get; set; }
 }
-
