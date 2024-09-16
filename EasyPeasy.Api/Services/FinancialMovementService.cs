75e89120406bbb2c69db991c52f9cb92863d9730
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-16 18:12:58 -0300

    feat: creating service for FinancialMovement

---
+++ EasyPeasy.Api/Services/FinancialMovementService.cs
@@ -0,0 +1,87 @@
+using AutoMapper;
+using EasyPeasy.Api.Data;
+using EasyPeasy.Api.Domain;
+using EasyPeasy.Api.Domain.Dtos;
+using EasyPeasy.Api.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyPeasy.Api.Services;
+
+public class FinancialMovementService : IFinancialMovementService
+{
+    private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public FinancialMovementService(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public async Task<List<FinancialMovementDTO>> GetAll()
+    {
+        var movements = await _context.FinancialMovements
+            .AsNoTracking()
+            .ToListAsync();
+        
+        var movementsDto = _mapper.Map<List<FinancialMovementDTO>>(movements);
+        
+        return movementsDto;
+    }
+
+    public async Task<FinancialMovementDTO?> GetById(long id)
+    {
+        var movement = await _context.FinancialMovements
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
+        
+        var movementDto = _mapper.Map<FinancialMovementDTO>(movement);
+        
+        return movementDto;
+    }
+
+    public async Task<List<FinancialMovementDTO>?> GetByCategory(long id)
+    {
+        var movements = await _context.FinancialMovements
+            .AsNoTracking()
+            .Where(x => x.CategotyId == id)
+            .ToListAsync();
+        
+        var movementsDto = _mapper.Map<List<FinancialMovementDTO>>(movements);
+        
+        return movementsDto;
+    }
+
+    public async Task<FinancialMovementDTO> Post(FinancialMovementDTO dto)
+    {
+        var movement = _mapper.Map<FinancialMovement>(dto);
+        
+        _context.FinancialMovements.Add(movement);
+        await _context.SaveChangesAsync();
+        var movementDto = _mapper.Map<FinancialMovementDTO>(movement);
+        
+        return movementDto;
+    }
+
+    public async Task<FinancialMovementDTO?> Put(FinancialMovementDTO dto)
+    {
+        var movement = _mapper.Map<FinancialMovement>(dto);
+        
+        _context.FinancialMovements.Update(movement);
+        await _context.SaveChangesAsync();
+        var movementDto = _mapper.Map<FinancialMovementDTO>(movement);
+        
+        return movementDto;
+    }
+
+    public async Task<FinancialMovementDTO?> Delete(FinancialMovementDTO dto)
+    {
+        var movement = _mapper.Map<FinancialMovement>(dto);
+        
+        _context.FinancialMovements.Remove(movement);
+        await _context.SaveChangesAsync();
+        var deletedFinancialMovement = _mapper.Map<FinancialMovementDTO>(movement);
+        
+        return deletedFinancialMovement;
+    }
+}
---
+++ EasyPeasy.Api/Services/Interfaces/IFinancialMovementService.cs
@@ -0,0 +1,13 @@
+using EasyPeasy.Api.Domain.Dtos;
+
+namespace EasyPeasy.Api.Services.Interfaces;
+
+public interface IFinancialMovementService
+{
+    Task<List<FinancialMovementDTO>> GetAll();
+    Task<FinancialMovementDTO?> GetById(long id);
+    Task<List<FinancialMovementDTO>?> GetByCategory(long id);
+    Task<FinancialMovementDTO> Post(FinancialMovementDTO dto);
+    Task<FinancialMovementDTO?> Put(FinancialMovementDTO dto);
+    Task<FinancialMovementDTO?> Delete(FinancialMovementDTO dto);
+}
