0c7d5eac76b91a41d5c16065f9abf6eb3daf3fe1
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-16 17:45:25 -0300

    feat: create CategoryController

---
+++ EasyPeasy.Api/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+using EasyPeasy.Api.Domain;
+using EasyPeasy.Api.Domain.Dtos;
+using EasyPeasy.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyPeasy.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CategoryController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var categories = await _categoryService.GetAll();
+        
+        return Ok(categories);
+    }
+
+    [HttpGet("/{id:long}", Name = "GetCategory")]
+    public async Task<IActionResult> GetById([FromRoute] long id)
+    {
+        var category = await _categoryService.GetById(id);
+        if (category == null) return NotFound();
+        
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CategoryDTO dto)
+    {
+        var category = await _categoryService.Post(dto);
+        
+        return new CreatedAtRouteResult("GetCategory", new { id = category.Id }, category);
+    }
+
+    [HttpPut("/{id:long}")]
+    public async Task<IActionResult> Update([FromRoute] long id, [FromBody] CategoryDTO dto)
+    {
+        var category = await _categoryService.GetById(id);
+        if(category == null) return NotFound();
+        
+        category = await _categoryService.Put(dto);
+        
+        return Ok(category);
+    }
+
+    [HttpDelete("/{id:long}")]
+    public async Task<IActionResult> Delete([FromRoute] long id)
+    {
+        var category = await _categoryService.GetById(id);
+        if(category == null) return NotFound();
+        
+        await _categoryService.Delete(category);
+        
+        return NoContent();
+    }
+}
