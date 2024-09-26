f18a95b3c75416bb154755cefa21b99a52840ee0
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-26 20:41:53 -0300

    feat: organazing dtos and adding validations

---
+++ EasyPeasy.Api/Domain/Dtos/Category/CategoryDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace EasyPeasy.Api.Domain.Dtos.Category;
+
+public class CategoryDTO
+{
+    public long Id { get; set; }
+    [Required(ErrorMessage = "Titulo é obrigatório")]
+    [MaxLength(80, ErrorMessage = "Título deve ter no máximo 80 caracteres")]
+    [MinLength(5, ErrorMessage = "Título deve ter no mínimo 5 caracteres")]
+    public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Descrição é obrigatória")]
+    [MaxLength(255, ErrorMessage = "Descrição deve ter no máximo 255 caracteres")]
+    [MinLength(10, ErrorMessage = "Descrição deve ter no mínimo 10 caracteres")]
+    public string? Description { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
+
+public class CategoryMinDTO
+{
+    [Required(ErrorMessage = "Titulo é obrigatório")]
+    [MaxLength(80, ErrorMessage = "Título deve ter no máximo 80 caracteres")]
+    [MinLength(5, ErrorMessage = "Título deve ter no mínimo 5 caracteres")]
+    public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Descrição é obrigatória")]
+    [MaxLength(255, ErrorMessage = "Descrição deve ter no máximo 255 caracteres")]
+    [MinLength(10, ErrorMessage = "Descrição deve ter no mínimo 10 caracteres")]
+    public string? Description { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+}
---
+++ EasyPeasy.Api/Domain/Dtos/FinancialMovement/FinancialMovementDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using EasyPeasy.Api.Domain.Enums;
+
+namespace EasyPeasy.Api.Domain.Dtos.FinancialMovement;
+
+public class FinancialMovementDTO
+{
+    public long Id { get; set; }
+    [Required(ErrorMessage = "Titulo é obrigatório")]
+    [MaxLength(80, ErrorMessage = "Título deve ter no máximo 80 caracteres")]
+    [MinLength(5, ErrorMessage = "Titulo deve ter no mínimo 5 caracteres")]
+    public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Valor é obrigatório")]
+    [Range(0, double.MaxValue, ErrorMessage = "Valor deve ser maior que zero")]
+    public decimal Amount { get; set; }
+    [JsonIgnore]
+    public DateTime CreatedAt { get; set; }
+    [Required(ErrorMessage = "Tipo é obrigatório")]
+    public EMovementType Type { get; set; }
+    [Required(ErrorMessage = "Categoria é obrigatória")]
+    public long CategoryId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+}
+
