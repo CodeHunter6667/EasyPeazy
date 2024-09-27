7f30195abfd6c838829ba2a11638a48b5d750f8d
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-09-26 21:22:17 -0300

    feat: creating extension methods to organize main program

---
+++ EasyPeasy.Api/Common/Api/AppExtension.cs
@@ -0,0 +1,37 @@
+﻿using EasyPeasy.Api.Domain;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace EasyPeasy.Api.Common.Api;
+
+public static class AppExtension
+{
+    public static void ConfigureDevEnvironment(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapSwagger().RequireAuthorization();
+    }
+
+    public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature != null)
+                {
+                    await context.Response.WriteAsync(new ErrorDetail()
+                    {
+                        StatusCode = context.Response.StatusCode,
+                        Message = contextFeature.Error.Message,
+                        Trace = contextFeature.Error.StackTrace
+                    }.ToString());
+                }
+            });
+        });
+    }
---
+++ EasyPeasy.Api/Common/Api/BuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using EasyPeasy.Api.Data;
+using EasyPeasy.Api.Services.Interfaces;
+using EasyPeasy.Api.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyPeasy.Api.Common.Api;
+
+public static class BuilderExtensions
+{
+    public static void AddConfigurations(this WebApplicationBuilder builder)
+    {
+        Configuration.ConnectionString = 
+            builder.
+            Configuration.
+            GetConnectionString("SqlConnection") ?? string.Empty;
+    }
+
+    public static void AddDataContext(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseNpgsql(Configuration.ConnectionString)
+        );
+    }
+
+    public static void AddAutoMapper(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    }
+
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ICategoryService, CategoryService>();
+        builder.Services.AddScoped<IFinancialMovementService, FinancialMovementService>();
+    }
+
+    public static void AddDocumentation(this WebApplicationBuilder builder)
+    {
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+    }
+}
